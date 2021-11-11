cde4f08704895fbd5352821ad75b6322ad218aea
Ruinan Jin <jinruinan@163.com>
2021-11-11 11:09:55 -0800

    Create register.php

---
+++ php/register.php
@@ -0,0 +1,9 @@
+<?php
+    $success = True; //keep track of errors so it redirects the page only if there are no errors
+    $db_conn = NULL; // edit the login credentials in connectToDB()
+    $show_debug_alert_messages = False; // set to True if you want alerts to show you which methods are being triggered (see how it is used in debugAlertMessage())
+    $username = $_REQUEST["username"];
+    $password = $_REQUEST["password"];
+
+    
+?>
