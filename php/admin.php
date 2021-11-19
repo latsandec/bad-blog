2c8925fc3c0d55abf47733106176a26c1c1f9f01
Annie <1000wangyiqian@gmail.com>
2021-11-19 15:26:22 -0800

    start

---
+++ php/admin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Powerful Admin</title>
+  <link rel="stylesheet" href="styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=MonteCarlo&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+
+
+
+
+
+<body>
+  <div class="top-container">
+    <img class="top-code" src="binary-code.png" alt="code-img">
+    <h1>Welcome</h1>
+    <h2 id: "admin">Our <span class="power">power</span>ful Admin</h2>
+    <img class="bottom-code" src="binary-code.png" alt="code-img">
+    <img class="programming-gif" src="programming.png" alt="animation-img">
+  </div>
+  <div>
+
+  </div>
+
+
+</body>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
--- php/admin_login.php
+++ php/admin_login.php
@@ -1,41 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-<body>
-    
-<form action="login.php" method="post">
-    <fieldset>
-        <legend>用户登录</legend>
-        <ul>
-            <li>
-                <label>用户名:</label>
-                <input type="text" name="username">
-            </li>
-            <li>
-                <label>密   码:</label>
-                <input type="password" name="password">
-            </li>
-            <li>
-                <label> </label>
-                <input type="checkbox" name="remember" value="yes">7天内自动登录
-            </li>
-            <li>
-                <label> </label>
-                <input type="submit" name="login" value="登录">
-            </li>
-			<li>
-                <label> </label>
-                <input type="submit" name="reset" value="reset">
-            </li>
-        </ul>
-    </fieldset>
-</form> -->
-
     <?php 
 	include "connect.php";
 	header('Content-type:text/html; charset=utf-8');
@@ -43,7 +5,6 @@
 	session_start();
 	
 	// retrive user data
-	
 	if(connectToDB()){
 		if (isset($_POST['login'])) {
 			
@@ -51,22 +12,8 @@
 			$password = trim($_POST['password']);
 			
 			$cmd = "SELECT * FROM blog_users WHERE userID = $username";
-			// echo "<br> .$cmd . <br>";
 			$result = executePlainSQL($cmd);
 			$row = OCI_Fetch_Array($result, OCI_BOTH);
-			// echo "<br> $row[3] <br>";
-			// echo var_dump($password);
-			// echo var_dump($row[3]);
-			// if(stringIsEqual($password,$row[3])){
-			// 	echo "True";
-			// }else{
-			// 	echo "False";
-			// }
-			// echo var_dump($password);
-			// while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-            //     "<tr><td>" . $row["COMMUNITYID"] . "</td><td>" . $row["TOPIC_NAME"] . "</td></tr>";
-            // }
-
 			OCICommit($db_conn);
 			
 			if (($username == '') || ($password == '')) {
