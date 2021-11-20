efbf0607bc910579449cfc8e96f7040aff0a74e0
Xueyi Yu <opkisky@students.cs.ubc.ca>
2021-11-20 03:11:46 -0800

    Update Topics in community_list.php, working on blog_list.php

--- php/community_list.php
+++ php/community_list.php
@@ -5,7 +5,7 @@
 
     <body>
         <h2>Enter Community ID to view Blogs</h2>
-        <form method="POST" action="community_list.php"> 
+        <form method="GET" action="community_list.php"> 
             <input type="hidden" id="viewCommunityRequest" name="viewCommunityRequest">
             ID: <input type="text" name="communityID"> <br /><br />
 
@@ -16,21 +16,23 @@
 
         <h2>Community List</h2>
         <p>If you want to view the community list, press on the view botton</p>
-        <form method="POST" action="community_list.php"> 
+        <form method="GET" action="community_list.php"> 
             <input type="hidden" id="listCommunityRequest" name="listCommunityRequest">
             <input type="submit" value="View" name="listSubmit"></p>
         </form>
         <?php
         include "connect.php";
-
+        session_start();
+        
         function handleViewRequest() {
-            $communityID = $_POST['communityID'];
+            $communityID = $_GET['communityID'];
                     
             if ($communityID !== '') {
                 $result = executePlainSQL("SELECT * FROM Community WHERE communityID = $communityID");
                 $row = OCI_Fetch_Array($result, OCI_BOTH); 
                 if ($row[0]){
-                    echo $row[0]." blog list to do";
+                    $_SESSION['community'] = $row[0];
+                    header("refresh:0;url=blog_list.php");
                 } else {
                     echo "<br>Result not found.<br>";
                 }
@@ -44,20 +46,26 @@
 
             echo "<br>Community List:<br>";
             echo "<table>";
-            echo "<tr><th>ID</th><th>Level</th></tr>";
+            echo "<tr><th>ID</th><th>Level</th><th>Topics</th></tr>";
 
             while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>"; 
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td>"; 
+                $communityID = $row[0];
+                $topics = executePlainSQL("SELECT topic_name FROM About WHERE communityID = $communityID");
+                while ($topic = OCI_Fetch_Array($topics, OCI_BOTH)) {
+                    echo "<td>" . $topic[0] . "</td>";
+                }
+                echo "</tr>";
             }
 
             echo "</table>";
         }
 
-        function handlePOSTRequest() {
+        function handleGETRequest() {
             if (connectToDB()) {
-                if (array_key_exists('viewCommunityRequest', $_POST)) {
+                if (array_key_exists('viewCommunityRequest', $_GET)) {
                     handleViewRequest();
-                } else if (array_key_exists('listCommunityRequest', $_POST)) {
+                } else if (array_key_exists('listCommunityRequest', $_GET)) {
                     handleListRequest();
                 }
 
@@ -65,8 +73,9 @@
             }
         }
 
-		if (isset($_POST['submit']) || isset($_POST['listSubmit'])) {
-            handlePOSTRequest();
+        session_start();
+		if (isset($_GET['submit']) || isset($_GET['listSubmit'])) {
+            handleGETRequest();
         } 
 
 	?>
