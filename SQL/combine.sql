16bf1376daad8034ed62dfa38a3bfc42e5824381
Ruinan Jin <jinruinan@163.com>
2021-11-25 23:32:19 -0800

    Update sql.

--- SQL/combine.sql
+++ SQL/combine.sql
@@ -1,3 +1,128 @@
+drop table VIP;
+drop table SecurityInfo_Of;
+drop table Ban;
+drop table Admin_Monitor;
+drop table Subscribe;
+drop table About;
+drop table Comment_Create_Follows;
+drop table BID;
+drop table UID_CID_DATETIME;
+drop table CID_DATETIME_Title;
+drop table Topic;
+drop table Blog_Users;
+drop table Community;
+
+CREATE TABLE Blog_Users(
+    userID INTEGER,
+    userName CHAR(250) NOT NULL,
+    birthday CHAR(10),
+    userPassword CHAR(250) NOT NULL,
+    ban_status INTEGER NOT NULL,
+    PRIMARY KEY(userID)
+);
+
+
+CREATE TABLE VIP(
+    vipID INTEGER,
+    vip_level INTEGER NOT NULL,
+    preferred_color CHAR(250),
+    PRIMARY KEY(vipID),
+    FOREIGN KEY(vipID) REFERENCES Blog_Users(userID) ON DELETE CASCADE
+);
+
+CREATE TABLE SecurityInfo_Of(
+    userID INTEGER NOT NULL,
+    email CHAR(250),
+    security_question CHAR(250) NOT NULL,
+    answer CHAR(250) NOT NULL,
+    PRIMARY KEY(email),
+    FOREIGN KEY(userID) REFERENCES Blog_Users(userID) ON DELETE CASCADE
+);
+
+CREATE TABLE Community(
+    communityID INTEGER,
+    community_level INTEGER NOT NULL,
+    PRIMARY KEY(communityID)
+);
+
+CREATE TABLE Admin_Monitor(
+    adminID INTEGER,
+    avatar CHAR(10) NOT NULL,
+    adminPassword CHAR(250) NOT NULL,
+    communityID INTEGER NOT NULL,
+    PRIMARY KEY(adminID),
+    FOREIGN KEY(communityID) REFERENCES Community(communityID) ON DELETE CASCADE
+);
+
+CREATE TABLE Ban(
+    userID INTEGER,
+    adminID INTEGER,
+    PRIMARY KEY(userID,adminID),
+    FOREIGN KEY(userID) REFERENCES Blog_Users(userID) ON DELETE CASCADE,
+    FOREIGN KEY(adminID) REFERENCES Admin_Monitor(adminID) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Topic(
+    topic_name CHAR(250),
+    PRIMARY KEY(topic_name)
+);
+
+CREATE TABLE Subscribe(
+    userID INTEGER,
+    communityID INTEGER,
+    PRIMARY KEY(userID,communityID),
+    FOREIGN KEY(userID) REFERENCES Blog_Users(userID) ON DELETE CASCADE,
+    FOREIGN KEY(communityID) REFERENCES Community(communityID) ON DELETE CASCADE
+);
+
+CREATE TABLE CID_DATETIME_Title(
+    communityID INTEGER,
+    DATETIME_record DATE,
+    title CHAR(250),
+    content CHAR(250) NOT NULL,
+    PRIMARY KEY(communityID,DATETIME_record,title),
+    FOREIGN KEY(communityID) REFERENCES Community(communityID) ON DELETE CASCADE
+);
+
+CREATE TABLE UID_CID_DATETIME(
+    userID INTEGER,
+    communityID INTEGER,
+    DATETIME_record DATE,
+    title CHAR(250) NOT NULL,
+    PRIMARY KEY(userID,communityID,DATETIME_record),
+    FOREIGN KEY(userID) REFERENCES Blog_Users(userID) ON DELETE CASCADE, 
+    FOREIGN KEY(communityID, DATETIME_record, title) REFERENCES CID_DATETIME_Title(communityID, DATETIME_record, title) ON DELETE CASCADE
+);
+
+CREATE TABLE BID(
+    blogId INTEGER,
+    userID INTEGER NOT NULL,
+    communityID INTEGER NOT NULL,
+    DATETIME_record DATE NOT NULL,
+    PRIMARY KEY(blogId),
+    FOREIGN KEY(userID,communityID, DATETIME_record) REFERENCES UID_CID_DATETIME(userID, communityID, DATETIME_record) ON DELETE CASCADE
+);
+
+CREATE TABLE Comment_Create_Follows(
+    comment_order INTEGER,
+    blogID INTEGER,
+    userID INTEGER NOT NULL,
+    content CHAR(250) NOT NULL,
+    PRIMARY KEY(comment_order,blogID),
+    FOREIGN KEY(blogID) REFERENCES BID(blogId) ON DELETE CASCADE,
+    FOREIGN KEY(userID) REFERENCES Blog_Users(userID) ON DELETE CASCADE
+);
+
+
+CREATE TABLE About(
+    communityID INTEGER,
+    topic_name CHAR(250),
+    PRIMARY KEY(communityID,topic_name),
+    FOREIGN KEY(communityID) REFERENCES Community(communityID) ON DELETE CASCADE,
+    FOREIGN KEY(topic_name) REFERENCES Topic(topic_name) ON DELETE CASCADE
+);
+
 INSERT INTO Blog_Users    VALUES(3040001,'Amy','2000-01-01','password3040001',0);
 INSERT INTO Blog_Users    VALUES(3040002,'Bob','2000-01-02','password3040002',1);
 INSERT INTO Blog_Users    VALUES(3040003,'Cindy','2000-01-03','password3040003',0);
@@ -109,5 +234,5 @@ INSERT INTO Comment_Create_Follows    VALUES(1,3042001,3040005,'Amazing!');
 INSERT INTO Comment_Create_Follows    VALUES(1,3042002,3040008,'check this link');
 INSERT INTO Comment_Create_Follows    VALUES(1,3042003,3040002,'lol');
 INSERT INTO Comment_Create_Follows    VALUES(1,3042004,3040005,'I also want one!!');
-INSERT INTO Comment_Create_Follows    VALUES(1,3042006,3040006,'Sorry to hear that'); --
-INSERT INTO Comment_Create_Follows    VALUES(2,3042006,3040004,'You will find a better job!'); --
+INSERT INTO Comment_Create_Follows    VALUES(1,3042006,3040006,'Sorry to hear that');
+INSERT INTO Comment_Create_Follows    VALUES(2,3042006,3040004,'You will find a better job!');
